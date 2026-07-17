5a323dc5d557e1dd5043c45c3973f146c88d4637
Antonio Caputo <brandradesilva13@gmail.com>
2026-07-17 00:32:13 -0300

    sync: entregas Husky 2026-07-16

--- src/lib/data/daily.ts
+++ src/lib/data/daily.ts
@@ -852,5 +852,12 @@ export const dailySeries = [
     "custo": 0,
     "fatura": 0,
     "margem": 0
+  },
+  {
+    "data": "2026-07-16",
+    "entregas": 35,
+    "custo": 0,
+    "fatura": 0,
+    "margem": 0
   }
 ];
--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -1,6 +1,6 @@
 export const meta = {
   periodoInicio: "2026-03-16",
-  periodoFim: "2026-07-15",
-  totalDias: 122,
+  periodoFim: "2026-07-16",
+  totalDias: 123,
   totalMotoristas: 201
 };
